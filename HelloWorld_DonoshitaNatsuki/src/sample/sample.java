9e06ccbc7bc8d0f4a5d341b82bc0c8f9293b30f8
堂之下奈月 <7kgotos@gmail.com>
2023-10-05 16:18:22 +0900

    1.3Java導入_1005

--- HelloWorld_DonoshitaNatsuki/src/sample/sample.java
+++ HelloWorld_DonoshitaNatsuki/src/sample/sample.java
@@ -6,14 +6,6 @@ public class sample {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello,World");
 		
-		// 自己紹介
-		String name = "堂之下 奈月";
-		int age = 24;
-		String hobby = "ゼルダのゲーム";
-
-		System.out.println("私の名前は" + name + "です。");
-		System.out.println("年齢は" + age + "歳です。");
-		System.out.println("趣味は" + hobby + "です。");
 	}
 
 }
